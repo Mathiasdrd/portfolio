50da42fd810b602230e843173088b697757aa74c
Mathias Deridder <deridder.mathias@hotmail.be>
2023-01-09 17:28:23 +0100

    homepage intro added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,13 @@ export default function Home() {
     return(
         <div className='container'>
             <h1>Homepage</h1>
+
+            <div>
+                <p>
+                    Welcome to my portfolio. This portfolio is a work in progress and will get improved regularly.
+                    New projects will be added to the portfolio when I make good progress on them or when they are finished.  
+                </p>
+            </div>
         </div>
     )
 }
